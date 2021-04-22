4a41d415ab4b9343f124d9c708b6024d0e61a090
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-22 14:25:06 -0500

    Implementando Workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,3 +8,10 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+// App Shell
+workbox.routing.registerNavigationRoute('/index.html')
+
+// Todo lo demás usa Network First
+workbox.routing.registerRoute(/^https?.*/,
+  workbox.strategies.networkFirst(), 'GET')
